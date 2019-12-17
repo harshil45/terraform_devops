71501910dc600955a40cb75d9d865918e7f00be6
Harshil Assudani <48145828+harshil45@users.noreply.github.com>
2019-12-17 17:01:54 +0530

    Update main.tf

--- devops123/main.tf
+++ devops123/main.tf
@@ -2,6 +2,19 @@ resource "azurerm_resource_group" "RG" {
   name     = "${var.Resource_Group}"
   location = "${var.location}"
 }
+resource "azurerm_virtual_network" "Vnet" {
+  name                = "vnet123"
+  location            = "${azurerm_resource_group.RG.location}"
+  resource_group_name = "${azurerm_resource_group.RG.name}"
+  address_space       = ["10.0.0.0/16"]
+}
+resource "azurerm_subnet" "Subnet" {
+name =                  "defaultsubnet" 
+address_prefix =        "10.0.1.0/24"  
+virtual_network_name =  "${azurerm_virtual_network.Vnet.name}"
+resource_group_name =   "${azurerm_resource_group.RG.name}"
+
+}
 
 resource "azurerm_storage_account" "functionstorageaccount" {
   name                     = "${var.functionapp_storage}"
