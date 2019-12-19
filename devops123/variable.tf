388f4de5db41aa7d53d5e8ee0c904e4f418998f9
Harshil Assudani <48145828+harshil45@users.noreply.github.com>
2019-12-19 13:51:04 +0530

    Update variable.tf

--- devops123/variable.tf
+++ devops123/variable.tf
@@ -1,31 +1,20 @@
-
-variable "Resource_Group" {
-    type ="string"
-    default = "harshilfuction"
-  
-}
-variable "location" {
-    type ="string"
-    default = "westus2"
-  
-}
-variable "functionapp_storage" {
-
-    type ="string"
-    default = "harsshilfunc34523"
-  
-}
-variable "App_Service_Plan" {
-    type ="string"
-    default ="harshilfunc1234"
-  
-}
-variable "SKU" {
-    type= "string"
-    default ="Standard"  
+variable "RG-1-Name" {
+  type    = "string"
+  default = "Harshil-ggs"
 }
 
-variable "Function_App" {
-    type = "string"
-    default = "harshilfunc321"
+variable "RG-1-Location" {
+  type    = "string"
+  default = "Central US"
 }
+
+variable "RG-Tags-1-Key" {
+  type    = "string"
+  default = "Resource-Type"
+}
+
+variable "RG-Tags-1-Value" {
+  type    = "string"
+  default = "Resource-Group"
+}
+
