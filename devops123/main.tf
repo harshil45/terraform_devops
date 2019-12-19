a2495bf3a0707ecea3cfe4ebd274998eeba00314
Harshil Assudani <48145828+harshil45@users.noreply.github.com>
2019-12-19 13:49:14 +0530

    Update main.tf

--- devops123/main.tf
+++ devops123/main.tf
@@ -1,44 +1,5 @@
-resource "azurerm_resource_group" "RG" {
-  name     = "${var.Resource_Group}"
-  location = "${var.location}"
-}
-resource "azurerm_virtual_network" "Vnet" {
-  name                = "vnet123"
-  location            = "${azurerm_resource_group.RG.location}"
-  resource_group_name = "${azurerm_resource_group.RG.name}"
-  address_space       = ["10.0.0.0/16"]
-}
-resource "azurerm_subnet" "Subnet" {
-name =                  "defaultsubnet" 
-address_prefix =        "10.0.1.0/24"  
-virtual_network_name =  "${azurerm_virtual_network.Vnet.name}"
-resource_group_name =   "${azurerm_resource_group.RG.name}"
-
-}
-
-resource "azurerm_storage_account" "functionstorageaccount" {
-  name                     = "${var.functionapp_storage}"
-  resource_group_name      = "${azurerm_resource_group.RG.name}"
-  location                 = "${azurerm_resource_group.RG.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service_plan" "Service-plan" {
-  name                = "${var.App_Service_Plan}"
-  location            = "${azurerm_resource_group.RG.location}"
-  resource_group_name = "${azurerm_resource_group.RG.name}"
-
-  sku {
-    tier = "${var.SKU}"
-    size = "S1"
-  }
-}
-
-resource "azurerm_function_app" "Function-App" {
-  name                      = "${var.Function_App}"
-  location                  = "${azurerm_resource_group.RG.location}"
-  resource_group_name       = "${azurerm_resource_group.RG.name}"
-  app_service_plan_id       = "${azurerm_app_service_plan.Service-plan.id}"
-  storage_connection_string = "${azurerm_storage_account.functionstorageaccount.primary_connection_string}"
+resource "azurerm_resource_group" "rg123"{
+name = "${var.RG-1-Name}"
+location = "${var.RG-1-Location}"
+tags = "${merge(map("${var.RG-Tags-1-Key}","${var.RG-Tags-1-Value}"))}" 
 }
